f075bc98846e95fd2d6f90ccaafe68939c657258
Saranya CR <saran@saranyas-air.lan>
2018-01-01 18:50:34 -0800

    commit all files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> GA APP </h1>
+        </div>
+    );
+  }
+}
+
+export default App;
